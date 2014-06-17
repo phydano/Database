b5dc1cb2700f4dca11ca19be6dff1de8afdf7162
phydano <ph.dano@gmail.com>
2014-06-17 16:48:09 +1200

    add new database

--- schema.js
+++ schema.js
@@ -5,6 +5,7 @@ var pg = require('pg').native
 
 client = new pg.Client(connectionString);
 client.connect();
-query = client.query('CREATE TABLE logindatabase (id integer , points integer)');
+//query = client.query('CREATE TABLE logindatabase (id integer , points integer)');
+query = client.query('CREATE TABLE mapsdatabase (title text, description text, longitude decimal(10,8) , latitude decimal(10,8), greenpoints integer)');
 query.on('end', function(result) { client.end(); });
 
